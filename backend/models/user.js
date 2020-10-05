c100d24f4d7315aca837bfd29dc38dcb46677e76
Khang On <javaneverdie0612@gmail.com>
2020-10-06 00:10:44 +0700

    setup project

---
+++ backend/models/user.js
@@ -0,0 +1,18 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true
+    },
+    email: DataTypes.STRING,
+    password: DataTypes.STRING
+  }, {});
+  User.associate = function (models) {
+
+  };
+  return User;
+};
+
+
